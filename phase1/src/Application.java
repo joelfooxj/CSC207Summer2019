53827852af1ce811c24fc9d040dbb8ac2fd064a2
shenjiab <jacob.shenjiabao@gmail.com>
2019-06-23 16:31:32 -0400

    application file 1st commit.

--- phase1/src/Application.java
+++ phase1/src/Application.java
@@ -1,7 +1,91 @@
 public class Application {
-
+    private long applicationID;
+    private long jobID;
+    private long applicantID;
+    private long firmID;
+    private int numInterview;
+    private long interviewerID;
+    private String cvPath;
+    private String clPath;
 
     public Application(long id){
+        this.applicantID = id;
+    }
 
+    public long getApplicationID() {
+        return applicationID;
+    }
+
+    public long getJobID() {
+        return jobID;
+    }
+
+    public long getApplicantID() {
+        return applicantID;
+    }
+
+    public long getFirmID() {
+        return firmID;
+    }
+
+    public int getNumInterview() {
+        return numInterview;
+    }
+
+    public long getInterviewerID() {
+        return interviewerID;
+    }
+
+    public String getCvPath() {
+        return cvPath;
+    }
+
+    public String getClPath() {
+        return clPath;
+    }
+
+    public void setApplicationID(long applicationID) {
+        this.applicationID = applicationID;
+    }
+
+    public void setJobID(long jobID) {
+        this.jobID = jobID;
+    }
+
+    public void setApplicantID(long applicantID) {
+        this.applicantID = applicantID;
+    }
+
+    public void setFirmID(long firmID) {
+        this.firmID = firmID;
+    }
+
+    public void setNumInterview(int numInterview) {
+        this.numInterview = numInterview;
+    }
+
+    public void setInterviewerID(long interviewerID) {
+        this.interviewerID = interviewerID;
+    }
+
+    public void setCvPath(String cvPath) {
+        this.cvPath = cvPath;
+    }
+
+    public void setClPath(String clPath) {
+        this.clPath = clPath;
+    }
+
+    @Override
+    public String toString() {
+        return
+                " applicationID: " + applicationID +
+                ", jobID: " + jobID +
+                ", applicantID: " + applicantID +
+                ", firmID: " + firmID + "\n"+
+                ", number of interviews taken " + numInterview +
+                ", the next interviewer has ID " + interviewerID + "\n"+
+                ", the path to the CV is " + cvPath +  " and the cover letter path is "+ clPath
+                ;
     }
 }
---
+++ phase1/src/ApplicationDatabase.java
@@ -0,0 +1,6 @@
+public class ApplicationDatabase {
+
+
+    public ApplicationDatabase() {
+    }
+}

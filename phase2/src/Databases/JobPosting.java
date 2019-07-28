5ff3bdd0ef6571fb5d496bdc36f9babbe20ae285
Admin <alec.mckay@mail.utoronto.ca>
2019-07-28 01:14:12 -0400

    Updating javadoc on JobPosting and Jobs Database. Added search by hashtag function to JobsDatabase and hashtag function for JobPosting.

--- phase2/src/Databases/JobPosting.java
+++ phase2/src/Databases/JobPosting.java
@@ -2,48 +2,110 @@ package Databases;
 
 import java.io.Serializable;
 import java.time.LocalDate;
+import java.util.Collection;
+import java.util.List;
 
-public class JobPosting extends JobsDatabase implements Serializable {
+public class JobPosting implements Serializable {
 
-    //question: is numberOfJobs redundant? Should I just copy it from the Databases.JobsDatabase currID?
+    /**
+     * The total number of JobPostings that have been created
+     */
     private static long numberOfJobs = 0;
 
-    private Long jobId;
-    private String jobTitle;
-    private String jobDetails;
-    private long firmid;
 
+    /**
+     * The id of the job, used to store and retrieve in databases
+     */
+    private Long jobId;
+
+    /**
+     * The title of the job
+     */
+    private String jobTitle;
+
+    /**
+     * The details and requirements of the job
+     */
+    private String jobDetails;
+
+    /**
+     * The id of the firm that is offering the job
+     */
+    private long firmId;
+
+    /**
+     * The date the job posting was published
+     */
     private LocalDate publishDate;
+
+    /**
+     * The date the job expires, where applications can no longer be submitted
+     */
     private LocalDate expiryDate;
+
+    /**
+     * A list of hashtags related to a job posting
+     */
+    private Collection<String> hashTags;
+
     //TODO: allow numLabourRequired to be changeable
+    /**
+     * The number of positions open for a job posting
+     */
     private int numLabourRequired = 1;
+
+    /**
+     * True all the job positions for a posting have been filled by candidates
+     */
     private boolean isFilled = false;
 
-    public JobPosting(String jobTitle, String jobDetails, long firmId, LocalDate publishDate, LocalDate expiryDate){
+    /**
+     *constructor for job postings
+     * @param jobTitle {@link #jobTitle}
+     * @param jobDetails - {@link #jobDetails}
+     * @param firmId - {@link #firmId}
+     * @param publishDate - {@link #publishDate}
+     * @param expiryDate - {@link #expiryDate}
+     */
+    //TODO code smell: too many paramaters
+    public JobPosting(String jobTitle, String jobDetails, long firmId, LocalDate publishDate, LocalDate expiryDate, Collection<String> hashTags){
         jobId = numberOfJobs;
         numberOfJobs ++;
         this.jobTitle = jobTitle;
         this.jobDetails = jobDetails;
-        this.firmid = firmId;
+        this.firmId = firmId;
         this.publishDate = publishDate;
         this.expiryDate = expiryDate;
+        this.hashTags = hashTags;
     }
 
-    //how do we check if a job has been filled?
-    public boolean isOpen(){
-        return !isFilled() && !isExpired();
+    /**
+     * Returns true if the job is not filled and has not expired
+     * @return true or false
+     */
+    public boolean isOpen(LocalDate todaysDate){
+        return !isFilled() && !isExpired(todaysDate);
     }
 
-    private boolean isExpired(){
+    /**
+     * Indicates whether the job has expired {@link #expiryDate}
+     * @return
+     */
+
+    private boolean isExpired(LocalDate todaysDate){
         //condition should be based on today's date
-        if(publishDate.compareTo(expiryDate) > 0){
+        if(todaysDate.compareTo(expiryDate) > 0){
             return true;
         }
         else{
             return false;
         }
     }
-    //what's the difference between isOpen() and isFilled()?
+
+    /**
+     * Indicates whether all the job positions have been filled or not
+     * @return {@link #isFilled}
+     */
     private boolean isFilled(){
         return isFilled;
     }
@@ -55,12 +117,38 @@ public class JobPosting extends JobsDatabase implements Serializable {
                 "Date Published: " + publishDate + "\n" + "Expiry Date: " + expiryDate + "\n";
     }
 
+    /**
+     * returns jobId
+     * @return {@link #jobId}
+     */
     public Long getJobId() {
         return jobId;
     }
 
-    public long getFirmid() {
-        return firmid;
+    /**
+     * returns firmId
+     * @return {@link #firmId}
+     */
+    public long getFirmId() {
+        return firmId;
+    }
+
+    /**
+     * Checks whether  any of the hashtags being searched forare contained in the hashtags associated
+     * with the job posting in the hashTags variable
+     * @param searchHashTags - a collection of hashtags being searched for
+     * @return true or false
+     */
+    public boolean containsHashTags(Collection<String> searchHashTags) {
+        System.out.println(searchHashTags);
+        searchHashTags.retainAll(hashTags);
+        System.out.println(searchHashTags);
+        if(!searchHashTags.isEmpty()){
+            return true;
+        }
+        else{
+            return false;
+        }
     }
 
 }

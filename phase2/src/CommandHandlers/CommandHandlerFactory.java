0b60b205e1b2f1e171ef5deaa63dc69675626619
user <abdennour.aissaoui@mail.utoronto.ca>
2019-07-28 19:03:28 -0400

    Design enhancement: renamed variables Added documentation to SaveFilesHandler

--- phase2/src/CommandHandlers/CommandHandlerFactory.java
+++ phase2/src/CommandHandlers/CommandHandlerFactory.java
@@ -5,13 +5,12 @@ import Databases.UserCredentials;
 public class CommandHandlerFactory {
 
 
-    /**
+    /** creates the appropriate commandhandler object given the UserCredentials object
      * @param userCredentials The user that is currently logged in
      * @return a commandHandler object
      */
     public CommandHandler getCommandHandler(UserCredentials userCredentials){
         CommandHandler commandHandler;
-        // set the value of CommandHandlers.CommandHandler based on the user type
         if (userCredentials.getUserType().equals(UserInterface.getApplicantUserType())){
             commandHandler = new ApplicantCommandHandler(userCredentials);
         } else if (userCredentials.getUserType().equals(UserInterface.getInterviewerUserType())){
